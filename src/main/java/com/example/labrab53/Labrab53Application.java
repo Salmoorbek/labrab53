05f08d28b10ed175f9dd75dd55f9987d4d6a8ad7
Salmoor Nurzhanov <salmor662@gmail.com>
2023-03-22 14:56:08 +0600

    start project

---
+++ src/main/java/com/example/labrab53/Labrab53Application.java
@@ -0,0 +1,13 @@
+package com.example.labrab53;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labrab53Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labrab53Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/labrab53/Labrab53ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labrab53;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labrab53ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
